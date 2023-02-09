130c0786e801dfa98d621f381cefca5cb38c98e1
Ali Abdunazarov <mukhammad.abdunazarov@gmail.com>
2023-02-10 00:31:39 +0200

    fngdf

--- java.js
+++ java.js
@@ -1,1 +1,23 @@
 "use strict"
+
+const numberOfSeries=prompt("Qaysi aktyorlarni taniysiz?" , "");
+
+const seriesDB={
+    count:{},
+    series:{},
+    actors:numberOfSeries,
+    genres:[],
+    privat:false,
+    rating:{},
+}
+ const a = prompt("Oxirgi ko’rgan aktyoringiz?"),
+ b=prompt("sizga uning ijrosi yoqdimi?"),
+ c=prompt("Oxirgi ko’rgan aktyoringiz?"),
+ d=prompt("sizga uning ijrosi yoqdimi?");
+
+
+ seriesDB.rating [a] = b;
+ seriesDB.rating [c] = d;
+
+ console.log(seriesDB);
+
